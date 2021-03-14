7e4e62f9d01be4eb8b6b36f9aced9d1f8ef5aade
Mike Fernando <mikera2020@outlook.com>
2021-03-14 17:29:20 -0300

    first commit

--- webpack.config.js
+++ webpack.config.js
@@ -13,14 +13,14 @@ module.exports = {
     filename: 'bundle.js'
   },
   resolve: {
-    extensions: ['.js', '.jsx', '.ts', '.tsx']
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
   },
   devServer: {
     contentBase: path.resolve(__dirname, 'public'),
     hot: true,
   },
   plugins: [
-    isDevelopment && new ReactRefreshWebpackPlugin(),  
+    isDevelopment && new ReactRefreshWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'public', 'index.html')
     })
